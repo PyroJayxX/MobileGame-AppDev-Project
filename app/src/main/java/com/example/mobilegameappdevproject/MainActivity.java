e86eb7e09d8f0eb9d9a5e4b5174b071ff292a765
edrill <edrillb@gmail.com>
2024-03-11 20:05:25 +0800

    - Added Animated Health Icons with Glide Dependency

--- app/src/main/java/com/example/mobilegameappdevproject/MainActivity.java
+++ app/src/main/java/com/example/mobilegameappdevproject/MainActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
-
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,11 +12,16 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.bumptech.glide.Glide;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private FrameLayout menuLayout;
     private FrameLayout mainGameLayout;
+    public ImageView hp_icon1;
+    public ImageView hp_icon2;
+    public ImageView hp_icon3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,14 +34,22 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        //Initialize
         menuLayout = findViewById(R.id.MenuLayout);
         mainGameLayout = findViewById(R.id.MainGameLayout);
+        hp_icon1 = findViewById(R.id.hp1);
+        hp_icon2 = findViewById(R.id.hp2);
+        hp_icon3 = findViewById(R.id.hp3);
     }
 
     // MENU Methods
     public void btnStart(View v){
         mainGameLayout.setVisibility(View.VISIBLE);
         menuLayout.setVisibility(View.GONE);
+
+        Glide.with(this).load(R.drawable.hp_icon).into(hp_icon1);
+        Glide.with(this).load(R.drawable.hp_icon).into(hp_icon2);
+        Glide.with(this).load(R.drawable.hp_icon).into(hp_icon3);
     }
 
     // MAIN GAME Methods
